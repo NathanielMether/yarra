144b5a1af70fec8ef5ca3a4fa04e117a5d362730
NathanielMether <nmet3@student.monash.edu>
2017-12-07 12:28:34 +1100

    Display user info wehn signed in

--- web/src/App.js
+++ web/src/App.js
@@ -2,25 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import SignInForm from './components/SignInForm'
 import { signIn } from './api/auth'
+import { listProducts} from './api/products'
+import { setToken } from './api/init'
 
 class App extends Component {
+  state = {
+    decodedToken: null
+  }
+
   onSignIn = ({ email, password}) => {
-    signIn({ email, password})
-      .then((data) => {
-        console.log('Signed In', data)
+    signIn({ email, password })
+      .then((decodedToken) => {    
+        this.setState({ decodedToken })
       })
   }
 
   render() {
+    const { decodedToken } = this.state
+
     return (
       <div className="App">
         <h1>Yarra</h1>
         <h2 className='mb-3'>Now delivering all your Nisal merchandise to your door!</h2>
-        <SignInForm
-          onSignIn={ this.onSignIn }
-        />
+        {
+          !!decodedToken ? (
+            <div>
+              <p>Email: { decodedToken.email }</p>
+              <p>Signed in at: { new Date(decodedToken.iat * 1000).toISOString() }</p>
+              <p>Expire at: { new Date(decodedToken.exp * 1000).toISOString() }</p>
+            </div>
+          ) : (
+            <SignInForm
+              onSignIn={ this.onSignIn }
+            />
+          )
+        }
       </div>
     );
+  }
+
+  componentDidMount() {
+    listProducts()
+      .then((products) => {
+        console.log(products)
+      })
+      .catch((error) => {
+        console.log('error loading products', error)
+      })
   }
 }
 
