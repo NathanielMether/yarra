e98d08640b130fa8d147150e25c20b55c147ddb2
NathanielMether <nmet3@student.monash.edu>
2017-12-07 11:03:37 +1100

    Add submision of sign in form

--- web/src/components/SignInForm.js
+++ web/src/components/SignInForm.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 function SignInForm({
-
+  onSignIn
 }) {
   return (
-    <form>
+    <form 
+      onSubmit={ (event) => {
+        // Prevent old-school form submission
+        event.preventDefault()
+        const elements = event.target.elements // get form attributes
+        const email = elements.email.value
+        const password = elements.password.value
+        onSignIn({ email, password})
+      }}
+    >
       <label className='mb-2'>
         {'Email: '}
         <input
