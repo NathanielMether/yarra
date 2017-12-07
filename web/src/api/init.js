0281b5aa5dead364e0899aa4f78eee126457a6d2
NathanielMether <nmet3@student.monash.edu>
2017-12-07 12:59:54 +1100

    save user token

--- web/src/api/init.js
+++ web/src/api/init.js
@@ -1,11 +1,22 @@
 import axios from 'axios'
+import { saveToken, getValidToken } from './token'
 
 const api = axios.create({
   baseURL: 'http://localhost:7000'
 })
 
 export function setToken(token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  saveToken(token)
+
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  } 
+  else {
+    delete api.defaults.headers.common['Authorization']
+  }
 }
+
+// Validates token and if it's invalid remove it from local storage
+setToken(getValidToken())
 
 export default api
